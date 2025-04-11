80988f8a5beddc489bde92539ec6055d8e792546
Kishore <kishorekumar1100011@gmail.com>
2025-04-11 16:01:29 +0530

    Typo error three

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col items-center mt-80">
       <div>
-        <p className="text-2xl">© Kishore Kumar 2025.</p>
+        <p className="text-2xl">© Kishore Kumar 2025</p>
       </div>
       <div className="text-xl pt-4 pb-8">
         <p>Github</p>
@@ -15,7 +15,7 @@ export default function Footer() {
       </div>
       <div>
         <p className="text-xl mx-7 mt-20 mb-30">
-          Like I always say, cant find a door? Make your own. Edward Elric, Fullmetal Alchemist
+          Like I always say cant find a door Make your own Edward Elric Fullmetal Alchemist
         </p>
       </div>
     </div>
