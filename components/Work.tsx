ec097af37985bb4235c05c822bf33d8724043cd8
Kishore <kishorekumar1100011@gmail.com>
2025-04-18 17:16:42 +0530

    Added work - flash drive

--- components/Work.tsx
+++ components/Work.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 export default function Work() {
   return (
-    <div className="grid grid-cols-2">
+    <div className="flex flex-col">
       {/* Work */}
       <div className="flex flex-col mx-18">
         <p className="text-5xl ml-10 mt-40 pb-14">Selected Works</p>
+
+        {/* Dash */}
         <p className="text-4xl pb-2">DASH</p>
         <p className="text-2xl text-[#A3A3A3] pb-4">Desktop App</p>
         <Image src="/images/dash.webp" alt="Dash" height={200} width={400} className="rounded-xl" />
@@ -29,9 +31,34 @@ export default function Work() {
         <div className="flex justify-end">
           <button className="mb-20 size-16 bg-gray-800 rounded-full">#</button>
         </div>
-        <div className="flex justify-center">
-          <button className="text-xl h-12 w-60 rounded-full bg-[#3D3D3D]"># discover more #</button>
+      </div>
+
+      {/* Flask Drive */}
+      <p className="text-4xl pb-2">FLASH DRIVE</p>
+      <p className="text-2xl text-[#A3A3A3] pb-4">Website</p>
+      <Image src="/images/dash.webp" alt="Dash" height={200} width={400} className="rounded-xl" />
+      <div className="flex flex-row justify-between mt-10">
+        <div>
+          <p className="text-sm text-stone-500">EVENT</p>
+          <p className="text-lg">Side Project</p>
         </div>
+        <div>
+          <p className="text-sm text-stone-500">YEAR</p>
+          <p className="text-lg">2023</p>
+        </div>
+      </div>
+      <p className="text-sm text-stone-500 pb-2 mt-12">DESCRIPTION</p>
+      <p className="text-lg mb-7">
+        A Next.js web platform featuring an AI image generator with custom-trained Stable Diffusion
+        model and a few other arcade games. Built with Firebase and EpicRealism image model,
+        it&apos;s just a DBMS course project that evolved into a playground for implementing
+        advanced web technologies.
+      </p>
+      <div className="flex justify-end">
+        <button className="mb-20 size-16 bg-gray-800 rounded-full">#</button>
+      </div>
+      <div className="flex justify-center">
+        <button className="text-xl h-12 w-60 rounded-full bg-[#3D3D3D]"># discover more #</button>
       </div>
 
       {/* Techstack */}
