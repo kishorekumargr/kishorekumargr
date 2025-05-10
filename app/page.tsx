09594a5ff9ca479cfea2a101a4babea35b188ad1
Kishore <kishorekumar1100011@gmail.com>
2025-05-10 23:42:40 +0530

    Added Font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="m-20">
-      <h1 className="font-black text-6xl pb-2">Kishore Kumar</h1>
-      <p className="text-4xl font-bold mb-20 ">Software Developer</p>
-      <h2 className="text-5xl font-semibold">Projects</h2>
+      <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-2">Kishore Kumar</h1>
+      <p className="text-xl font-bold mb-20 ">Software Developer</p>
+      <h2 className="text-xl font-semibold">Projects</h2>
     </main>
   );
 }
