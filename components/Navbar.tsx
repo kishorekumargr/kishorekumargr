1c032aa176c8b93649b0ffe4a8f008f28a1ba564
Kishore <kishorekumar1100011@gmail.com>
2025-05-20 11:11:42 +0530

    Solved error link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,14 +5,12 @@ export default function Navbar() {
   return (
     <nav>
       <Theme />
-      {/* Mobile */}
-      {/* Desktop */}
       <div className="nav-desktop">
         <Link href="/">Home</Link>
         <Link href="/blog">Blog</Link>
-        <Link href="/snippets">Snippets</Link>
-        <Link href="/Resources">Resources</Link>
         <Link href="/projects">Projects</Link>
+        <Link href="/snippets">Snippets</Link>
+        <Link href="/">Resources</Link>
       </div>
     </nav>
   );
