3965b65daf38ac6c04b8347f71a879a4dcb1bebf
Kishore <kishorekumar1100011@gmail.com>
2025-06-13 17:36:43 +0530

    add: projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,11 +3,17 @@ export default function Projects() {
     <main>
       <div>
         <h1>Projects</h1>
-        <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
+        <div className="shadow-lg hover:shadow-xl mt-10 mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
           <p className="font-bold text-xl">Ecommerce</p>
         </div>
         <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
-          <p className="font-bold text-xl">Socialmedia</p>
+          <p className="font-bold text-xl">Social Media</p>
+        </div>
+        <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
+          <p className="font-bold text-xl">Finance</p>
+        </div>
+        <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
+          <p className="font-bold text-xl">Real Estate</p>
         </div>
       </div>
     </main>
