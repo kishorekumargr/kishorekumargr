47ac5c38677e45927ea125d55fe57473511a56de
Kishore <kishorekumar1100011@gmail.com>
2025-06-19 09:48:52 +0530

    fix: projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,18 +9,6 @@ export default function Projects() {
         <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
           <p className="font-bold text-xl">Social Media</p>
         </div>
-        <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
-          <p className="font-bold text-xl">Finance</p>
-        </div>
-        <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
-          <p className="font-bold text-xl">Real Estate</p>
-        </div>
-        <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
-          <p className="font-bold text-xl">Clothing</p>
-        </div>
-        <div className="shadow-lg hover:shadow-xl mb-10 p-10 border border-gray-300 hover:border-gray-400 rounded-2xl w-md h-60">
-          <p className="font-bold text-xl">Cosmetics</p>
-        </div>
       </div>
     </main>
   );
